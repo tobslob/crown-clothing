c0bfef165a46618028b7060d7ded8ba5e94ffa99
tobslob <kazmobileapp@gmail.com>
2019-08-03 08:40:39 +0100

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import HomePage from './Homepage.component'
+import HomePage from './components/pages/homepage/Homepage.component'
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatPage = (props) => (
+  <div>
+    {console.log(props)}
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
