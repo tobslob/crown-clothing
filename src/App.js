1c887fa4ecdd69954e340c9aad8d3e972e9707d3
tobslob <kazmobileapp@gmail.com>
2019-08-04 03:17:07 +0100

    add header to app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css'
 
 import HomePage from './components/pages/homepage/Homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
