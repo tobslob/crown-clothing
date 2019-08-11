8c50699192a8410026f05fbbc7d208e07c2f7acb
tobslob <kazmobileapp@gmail.com>
2019-08-11 15:28:18 +0100

    update shop data and add cart utils to group item in cart reducer

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const cartActionTypes = {
-    TOGGLE_CARD_HIDDEN: 'TOGGLE_CARD_HIDDEN'
+    TOGGLE_CARD_HIDDEN: 'TOGGLE_CARD_HIDDEN',
+    ADD_ITEMS: 'ADD_ITEMS'
 }
 
 export default cartActionTypes;
